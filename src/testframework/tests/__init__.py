186fc1425ccf22d8cdd51612c2f686be694bc193
Tomer Gabay <tomergabay001@gmail.com>
2024-06-15 23:07:43 +0200

    added CorrectValue

--- src/testframework/tests/__init__.py
+++ src/testframework/tests/__init__.py
@@ -1,5 +1,12 @@
 from .category import ValidCategory
 from .range import ValidNumericRange
 from .regex import IsIntegerString, RegexTest
+from .value import CorrectValue
 
-__all__ = ["RegexTest", "IsIntegerString", "ValidNumericRange", "ValidCategory"]
+__all__ = [
+    "RegexTest",
+    "IsIntegerString",
+    "ValidNumericRange",
+    "ValidCategory",
+    "CorrectValue",
+]
---
+++ src/testframework/tests/value.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from pyspark.sql import Column, DataFrame
+from pyspark.sql import functions as F
+
+from testframework.base import Test
+from testframework.utils.decorators import account_for_nullable
+
+
+class CorrectValue(Test):
+    """
+    Returns True if a value is the correct value.
+    """
+
+    def __init__(self, *, name: str = "CorrectValue", correct_value: Any):
+        super().__init__(name=name)
+        self.correct_value = correct_value
+
+    @account_for_nullable
+    def _test_impl(self, df: DataFrame, column: str, nullable: bool) -> Column:
+        if nullable is False and self.correct_value is None:
+            raise ValueError(
+                "Nullable is False doesn't make sense if correct_value=None, please use nullable=True"
+            )
+
+        if self.correct_value is None:
+            return F.col(column).isNull()
+
+        return F.col(column) == self.correct_value
+
+    def __str__(self) -> str:
+        return f"{self.name}(correct_value={self.correct_value})"
+
+    def __repr__(self) -> str:
+        return f"{self.name}(correct_value={self.correct_value})"
