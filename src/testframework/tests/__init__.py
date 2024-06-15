f7e35ffcd5a7d9d04dbcb5b840772d8accbcce4c
Tomer Gabay <tomergabay001@gmail.com>
2024-06-15 10:52:38 +0200

    created ValidCategory

--- src/testframework/tests/__init__.py
+++ src/testframework/tests/__init__.py
@@ -1,3 +1,4 @@
+from .category import ValidCategory
 from .regex import IsIntegerString, RegexTest
 
-__all__ = ["RegexTest", "IsIntegerString"]
+__all__ = ["RegexTest", "IsIntegerString", "ValidCategory"]
---
+++ src/testframework/tests/category.py
@@ -0,0 +1,29 @@
+from pyspark.sql import Column, DataFrame
+from pyspark.sql import functions as F
+from pyspark.sql.types import StringType
+
+from testframework.base import Test
+from testframework.utils.decorators import account_for_nullable, allowed_col_types
+
+
+class ValidCategory(Test):
+    """
+    Returns True if the value is one of the possible categorical values.
+    """
+
+    def __init__(self, *, categories: set[str], name: str = "ValidCategory"):
+        super().__init__(name=name)
+        if isinstance(categories, list):
+            categories = set(categories)
+        if not isinstance(categories, set):
+            raise TypeError(f"Categories must be a set of strings. {categories = }")
+        self.categories = categories
+
+    @account_for_nullable
+    @allowed_col_types([StringType])
+    def _test_impl(self, df: DataFrame, column: str, nullable: bool) -> Column:
+        if nullable is False and None in self.categories:
+            raise ValueError(
+                "Nullable is False doesn't make sense if correct_values contains None, please use nullable=True"
+            )
+        return F.col(column).isin(self.categories)
