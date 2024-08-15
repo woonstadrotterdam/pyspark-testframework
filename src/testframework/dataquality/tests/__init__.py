1d942c00b698a05e81e5323bf9353d8635c02ff2
Tomer Gabay <tomergabay001@gmail.com>
2024-08-15 13:38:31 +0200

    added NotNull test

--- src/testframework/dataquality/tests/__init__.py
+++ src/testframework/dataquality/tests/__init__.py
@@ -1,5 +1,6 @@
 from .correct_value import CorrectValue
 from .integer_string import IntegerString
+from .not_null import NotNull
 from .regex_tst import RegexTest
 from .valid_category import ValidCategory
 from .valid_numeric_range import ValidNumericRange
@@ -8,6 +9,7 @@ from .valid_numeric_string_range import ValidNumericStringRange
 __all__ = [
     "CorrectValue",
     "IntegerString",
+    "NotNull",
     "RegexTest",
     "ValidCategory",
     "ValidNumericRange",
