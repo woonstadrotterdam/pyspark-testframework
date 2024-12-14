7ba9af5494d6c462c67284805267ad022f0dc4a2
Tomer Gabay <tomergabay001@gmail.com>
2024-12-14 14:19:40 +0100

    added ValidDateRange

--- src/testframework/dataquality/tests/__init__.py
+++ src/testframework/dataquality/tests/__init__.py
@@ -3,6 +3,7 @@ from .integer_string import IntegerString
 from .not_null import NotNull
 from .regex_tst import RegexTest
 from .valid_category import ValidCategory
+from .valid_date_range import ValidDateRange
 from .valid_numeric_range import ValidNumericRange
 from .valid_numeric_string_range import ValidNumericStringRange
 
@@ -12,6 +13,7 @@ __all__ = [
     "NotNull",
     "RegexTest",
     "ValidCategory",
+    "ValidDateRange",
     "ValidNumericRange",
     "ValidNumericStringRange",
 ]
